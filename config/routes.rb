07473de4cb09b2da4870e9f5793b1c9fce8692a5
Erick Rivera <yomellamoerick.771@gmail.com>
2023-07-04 18:53:21 -0400

    devise views added and comment scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   resources :publications
   devise_for :users
   get 'homes/index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_04_010943) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_04_225151) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id", null: false
+    t.bigint "publication_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["publication_id"], name: "index_comments_on_publication_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "publications", force: :cascade do |t|
     t.string "image"
@@ -32,9 +42,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_04_010943) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone"
+    t.string "name"
+    t.integer "age"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "publications"
+  add_foreign_key "comments", "users"
   add_foreign_key "publications", "users"
 end
