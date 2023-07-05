fdad6760acab33d7b617745887003bb6a4f4b42b
Erick Rivera <yomellamoerick.771@gmail.com>
2023-07-04 20:14:03 -0400

    ready for next steps

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :publications
+  has_many :comments
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
